7462fda3b94ed5fa6b7ad255fffd6c6b99d3d0c3
jooseppi12 <urijozsef@europe.com>
2021-12-07 22:05:44 +0100

    Job form wip

--- src/Dynamic/Client.fs
+++ src/Dynamic/Client.fs
@@ -166,11 +166,213 @@ module Contact =
                     ()
             ))
 
+module Jobs =
+    open WebSharper.UI
+    open WebSharper.UI.Client
+    open WebSharper.UI.Html
+
+    let totalMax = 10 * 1024 * 1024
+    let singleMax = 5 * 1024 * 1024
+    let button = JS.Document.GetElementById "JobSend"
+    let files : UI.ListModel<string, string * Blob> = UI.ListModel.Create fst []
+    files.View.Map(fun items ->
+        files.Doc(fun key _ ->
+            div [] [span [attr.``class`` "fileName"] [text key]; span [on.click (fun _ _ -> files.RemoveByKey key)] [text "x"]]
+        )
+    )
+    |> Doc.EmbedView
+    |> Doc.RunById "JobFiles"
+    let currentSize =
+        files.View
+        |> UI.View.Map (fun items ->
+            items
+            |> Seq.map snd
+            |> Seq.sumBy (fun x -> x.Size)
+        )
+
+    let fileHandle currentSize (fl: FileList) =
+        let mutable singleError = None
+        let mutable totalError = false
+        let mutable fileSize = 0
+        for i in [0..fl.Length - 1] do
+            let file = fl.Item i
+            if singleError = None && file.Size > singleMax then
+                singleError <- Some file.Name
+            fileSize <- fileSize + file.Size
+            
+        if singleError = None && currentSize + fileSize > totalMax then
+            totalError <- true
+        match singleError with
+        | None ->
+            if totalError then
+                let modal = JS.Document.QuerySelector "#JobSendFiles .modal"
+                let modalClose = JS.Document.QuerySelector "#JobSendFiles .modal .modal-button"
+                let modalContent = JS.Document.QuerySelector "#JobSendFiles .modal .modal-content"
+                modalContent.TextContent <- "The total file size have exceeded 10MB!"
+                modalClose.AddEventListener("click", System.Action<Dom.Event>(fun _ ->
+                    let modal = JS.Document.QuerySelector "#JobSendFiles .modal"
+                    modal.ClassList.Add "hidden"
+                    button.RemoveAttribute("disabled")
+                    button.ClassList.Remove("btn-disabled")
+                ))
+                modal.ClassList.Remove "hidden"
+            else
+                for i in [0..fl.Length - 1] do
+                    let file = fl.Item i
+                    files.Add (file.Name, (file :> Blob))
+        | Some fileName ->
+            let modal = JS.Document.QuerySelector "#JobSendFiles .modal"
+            let modalClose = JS.Document.QuerySelector "#JobSendFiles .modal .modal-button"
+            let modalContent = JS.Document.QuerySelector "#JobSendFiles .modal .modal-content"
+            modalClose.AddEventListener("click", System.Action<Dom.Event>(fun _ ->
+                let modal = JS.Document.QuerySelector "#JobSendFiles .modal"
+                modalContent.TextContent <- sprintf "%s is larger then 5MB" fileName
+                modal.ClassList.Add "hidden"
+                button.RemoveAttribute("disabled")
+                button.ClassList.Remove("btn-disabled")
+            ))
+            modal.ClassList.Remove "hidden"
+
+    let SendJobAction () =
+        let jobForm = JS.Document.GetElementById "JobSendFiles"
+        if jobForm <> null && jobForm <> JS.Undefined then
+            jobForm.AddEventListener("submit", System.Action<Dom.Event>(fun ev ->
+                ev.PreventDefault()
+            ))
+            let hiddenInput = JS.Document.QuerySelector "#JobSendFiles #hidden-input"
+            let fileUploadButton = JS.Document.QuerySelector "#JobSendFiles #button"
+            fileUploadButton.AddEventListener("click", System.Action<Dom.Event>(fun _ ->
+                hiddenInput?click()
+            ))
+            hiddenInput.AddEventListener("change", System.Action<Dom.Event>(fun ev ->
+                let ev = ev :?> ProgressEvent
+                currentSize
+                |> UI.View.Get (fun currentSize ->
+                    let fl = FileList.OfEvent ev
+                    fileHandle currentSize fl
+                )
+            ))
+
+            let jobUpload = JS.Document.GetElementById "JobUploadArea"
+            let dropHandler (ev: Dom.Event) =
+                ev.PreventDefault()
+                currentSize
+                |> UI.View.Get (fun cs ->
+                    fileHandle cs ((ev?dataTransfer?files) :> FileList)
+                )
+                jobUpload.ClassList.Remove "draggedOver"
+            let dragLeaveHandler (ev: Dom.Event) =
+                ev.PreventDefault()
+                jobUpload.ClassList.Remove "draggedOver"
+                //if ((ev?dataTransfer?files) :> FileList).Length > 0 then
+                //    jobUpload.ClassList.Remove "draggedOver"
+            let dragEnterHandler (ev: Dom.Event) =
+                ev.PreventDefault()
+                jobUpload.ClassList.Add "draggedOver"
+            let dragOverHandler (ev: Dom.Event) =
+                ev.PreventDefault()
+                ev?dataTransfer?dropEffect <- "copy"
+                Console.Log(((ev?dataTransfer?files) :> FileList).Length)
+            jobUpload.AddEventListener("dragleave", dragLeaveHandler)
+            jobUpload.AddEventListener("dragenter", dragEnterHandler)
+            jobUpload.AddEventListener("dragover", dragOverHandler)
+            jobUpload.AddEventListener("drop", dropHandler)
+            ()
+            
+        if button <> null && button <> JS.Undefined then
+            button.AddEventListener("click", System.Action<Dom.Event>(fun ev -> 
+                ev.PreventDefault()
+                let emailInput = JS.Document.QuerySelector "#JobSendFiles-form *[name=\"email\"]" :?> HTMLInputElement
+                let nameInput = JS.Document.QuerySelector "#JobSendFiles *[name=\"name\"]" :?> HTMLInputElement
+                let githubInput = JS.Document.QuerySelector "#JobSendFiles *[name=\"message\"]" :?> HTMLInputElement
+        
+                emailInput.ClassList.Remove("input-failed-validation")
+                emailInput.NextElementSibling
+                |> Optional.toOption
+                |> Option.iter (fun x -> x.ClassList.Add("hidden"))
+                nameInput.ClassList.Remove("input-failed-validation")
+                nameInput.NextElementSibling
+                |> Optional.toOption
+                |> Option.iter (fun x -> x.ClassList.Add("hidden"))
+
+                let email : string = emailInput.Value
+                let name : string = nameInput.Value
+                let github : string = githubInput.Value
+
+                if emailInput.Validity?typeMismatch || email.Trim() = "" then
+                    emailInput.ClassList.Add("input-failed-validation")
+                    emailInput.NextElementSibling
+                    |> Optional.toOption
+                    |> Option.iter (fun x -> x.ClassList.Remove("hidden"))
+                if name.Trim() = "" then
+                    nameInput.ClassList.Add("input-failed-validation")
+                    nameInput.NextElementSibling
+                    |> Optional.toOption
+                    |> Option.iter (fun x -> x.ClassList.Remove("hidden"))
+
+                if not emailInput.Validity?typeMismatch && name.Trim() <> "" then
+                    button.SetAttribute("disabled", "disabled")
+                    button.ClassList.Add("btn-disabled")
+                    let fd = FormData()
+                    fd.Append("email", email)
+                    fd.Append("name", name)
+                    if github.Trim() <> "" then
+                        fd.Append("github", github)
+                    files.Iter(fun (name, data) -> 
+                        fd.Append("files", data, name)
+                    )
+                    let options =
+                        RequestOptions(
+                            Method = "POST",
+                            Body = fd
+                        )
+                    let responsePromise = 
+                        JS.Fetch("https://api.intellifactory.com/api/jobs", options)
+                    responsePromise
+                        .Then(fun resp ->
+                            let modal = JS.Document.QuerySelector "#JobSendFiles .modal"
+                            let modalClose = JS.Document.QuerySelector "#JobSendFiles .modal .modal-button"
+                            modalClose.AddEventListener("click", System.Action<Dom.Event>(fun _ ->
+                                let modal = JS.Document.QuerySelector "#JobSendFiles .modal"
+                                emailInput.Value <- ""
+                                nameInput.Value <- ""
+                                githubInput.Value <- ""
+                                files.Clear()
+                                modal.ClassList.Add "hidden"
+                                button.RemoveAttribute("disabled")
+                                button.ClassList.Remove("btn-disabled")
+                            ))
+                            modal.ClassList.Remove "hidden"
+                        )
+                        .Catch(fun _ ->
+                            let modalContentText = "Sorry, we could not send your job application!"
+                            let modal = JS.Document.QuerySelector "#JobSendFiles .modal"
+                            let modalClose = JS.Document.QuerySelector "#JobSendFiles .modal .modal-button"
+                            let modalContent = JS.Document.QuerySelector "#JobSendFiles .modal .modal-content"
+                            modalContent.TextContent <- modalContentText
+                            modalClose.AddEventListener("click", System.Action<Dom.Event>(fun _ ->
+                                let modal = JS.Document.QuerySelector "#JobSendFiles .modal"
+                                emailInput.Value <- ""
+                                nameInput.Value <- ""
+                                githubInput.Value <- ""
+                                files.Clear()
+                                modal.ClassList.Add "hidden"
+                                button.RemoveAttribute("disabled")
+                                button.ClassList.Remove("btn-disabled")
+                            ))
+                            modal.ClassList.Remove "hidden"
+                        )
+                        |> ignore
+                else
+                    ()
+            ))
+
 [<SPAEntryPoint>]
 let Main() =
     Highlight.Run()
     Newsletter.SignUpAction()
     Contact.SendMessageAction()
+    Jobs.SendJobAction ()
 
 [<assembly:JavaScript>]
 do ()
